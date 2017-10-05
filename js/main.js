17a7335a9fc0d65c9d036884210d75df89045974
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-10-05 11:46:26 -0400

    finish light-switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+var text = document.querySelector('.status')
+var bodyLight = document.querySelector('body')
+bodyLight.style.backgroundColor = '#DBF3DF'
 
+var button = document.querySelector('button')
+button.addEventListener('click', function(){
+ if (button.className === 'switch on'){
+  button.className = 'switch off';
+  bodyLight.style.backgroundColor = 'black';
+  text.innerText = "Night Time";
+  text.style.color = 'white';
+  }
 
+  else if (button.className === 'switch off') {
+    button.className = 'switch on';
+    bodyLight.style.backgroundColor = '#DBF3DF';
+    text.innerHTML = "Day Time";
+    text.style.color = 'black';
+  }
+
+});
